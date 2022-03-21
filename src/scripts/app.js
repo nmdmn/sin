d95ae5f81dc6603c2bd3ef8bf0da82601d972aca
nmd <afarago.nmd@gmail.com>
2022-03-21 12:19:40 +0100

    ADD webgl renderer

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,44 @@
+import * as DAT from "dat.gui";
+import * as THREE from "three";
+
 export default class App {
-  constructor(gui) {
+  constructor(callback) {
     this.settings = {
       someSettingFrom0To1 : 0,
+      clearColor : 0x121212,
     };
-    this.gui = gui;
+    callback(this.settings);
+    this.gui = new DAT.GUI();
     this.gui.add(this.settings, "someSettingFrom0To1", 0, 1, 0.1);
+    this.scene = new THREE.Scene();
+    this.aspectRatio = window.innerWidth / window.innerHeight;
+    this.camera = new THREE.PerspectiveCamera(42, this.aspectRatio, 0.1, 1000);
+    this.canvas = document.querySelector("canvas");
+    this.renderer = new THREE.WebGLRenderer({canvas : this.canvas});
+    this.renderer.setClearColor(this.settings.clearColor);
+    this.onWindowResize();
+    this.setEventListeners();
+  }
+
+  update() {
+    this.updateCallback(42);
+    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(this.update.bind(this));
+  }
+
+  start(callback) {
+    this.updateCallback = callback;
+    this.update();
+  }
+
+  setEventListeners() {
+    window.addEventListener('resize', () => { this.onWindowResize(); });
+  }
+
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
   }
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,15 @@
-import * as DAT from "dat.gui";
-import * as THREE from "three";
-
 import App from "./app.js";
 
 function main() {
-  console.log("~!@#$%^&*()_+DEADBEEF");
-  const gui = new DAT.GUI();
-  const app = new App(gui);
+  const app = new App(settings => {
+    // setup
+    settings["clearColor"] = 0x121212;
+  });
+
+  app.start(dT => {
+    // update
+    console.log(dT);
+  });
 }
 
 main();
