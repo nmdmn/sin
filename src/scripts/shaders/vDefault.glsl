cbb65904f4f541adad2e6068b1a51539b163ea0b
nmd <afarago.nmd@gmail.com>
2022-03-24 01:59:28 +0100

    MOD animation for single wave through the geom.

--- src/scripts/shaders/vDefault.glsl
+++ src/scripts/shaders/vDefault.glsl
@@ -8,7 +8,7 @@ uniform float scroll;
 void main() {
   vUv = uv;
   float wobbleCoef =
-      smoothstep(-0.5, 3., sin(time * distance(vec2(.0, .0), uv)));
+      smoothstep(-0.5, 3., sin(time + distance(vec2(.0, .0), uv)));
   vec3 displacedPos = vec3(position.xy, wobbleCoef * (1. - random));
   vec4 worldPosition = modelViewMatrix * vec4(displacedPos, 1.);
   gl_PointSize = (9. * (1. / -worldPosition.z) * random * 2.) + 3.;
