4a719618a8f8115d74e824a57c9398d060e32fb5
nmd <afarago.nmd@gmail.com>
2022-03-28 20:03:59 +0200

    MOD fancy particle moving

--- src/scripts/shaders/fDefault.glsl
+++ src/scripts/shaders/fDefault.glsl
@@ -1,7 +1,16 @@
 varying vec2 vUv;
 varying float vNoise;
 
+float map(float value, float min1, float max1, float min2, float max2) {
+  return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+}
+
 void main() {
   float alpha = 1. - smoothstep(-0.2, 0.5, length(gl_PointCoord - vec2(.5)));
-  gl_FragColor = vec4(vec3(vNoise), alpha);
+  float phi = vNoise * 2. * 3.1614;
+  float theta = 1. / vNoise * 3.1614;
+  float r = map(sin(theta) * cos(phi), -1., 1., .0, 1.);
+  float g = map(sin(theta) * sin(phi), -1., 1., .0, 1.);
+  float b = map(cos(phi), -1., 1., .0, 1.);
+  gl_FragColor = vec4(vec3(r, g, b), alpha);
 }
--- src/scripts/shaders/vDefault.glsl
+++ src/scripts/shaders/vDefault.glsl
@@ -6,13 +6,25 @@ varying float vNoise;
 uniform float time;
 uniform float scroll;
 
+float map(float value, float min1, float max1, float min2, float max2) {
+  return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+}
+
 void main() {
   vUv = uv;
   vNoise = noise;
 
-  float alpha = 10. * smoothstep(-5., 5., (time / 2. * (1. / noise)));
-  vec3 displaceZ = vec3(.0, .0, smoothstep(-.03, .03, sin(alpha)) / 4.);
-  vec4 worldPosition = modelViewMatrix * vec4(displaceZ + position, 1.);
+  const float animLength = 7.;
+  float animTime = mod(time, animLength) / animLength;
+  float animOffset = time + noise;
+  float theta = animOffset / 2.;
+  float phi = animOffset / 7.;
+  float x = smoothstep(-.05, .05, sin(theta) * cos(phi));
+  float y = smoothstep(-.05, .05, sin(theta) * sin(phi));
+  float z = smoothstep(-.05, .05, cos(theta));
+  const float scale = 0.25;
+  vec3 newPosition = position + vec3(x, y, z) * scale;
+  vec4 worldPosition = modelViewMatrix * vec4(newPosition, 1.);
   gl_PointSize = (75. * (1. / -worldPosition.z) * noise) + 3.;
   gl_Position = projectionMatrix * worldPosition;
 }
