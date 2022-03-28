11efeedc1be029449c1f0a9f5f12ff1b3d62618b
nmd <afarago.nmd@gmail.com>
2022-03-28 20:08:14 +0200

    FIX anim.

--- src/scripts/shaders/vDefault.glsl
+++ src/scripts/shaders/vDefault.glsl
@@ -16,13 +16,13 @@ void main() {
 
   const float animLength = 7.;
   float animTime = mod(time, animLength) / animLength;
-  float animOffset = time + noise;
-  float theta = animOffset / 2.;
+  float animOffset = time + 1. / noise;
+  float theta = animOffset / 3.;
   float phi = animOffset / 7.;
-  float x = smoothstep(-.05, .05, sin(theta) * cos(phi));
-  float y = smoothstep(-.05, .05, sin(theta) * sin(phi));
-  float z = smoothstep(-.05, .05, cos(theta));
-  const float scale = 0.25;
+  float x = smoothstep(-.03, .03, sin(theta) * cos(phi));
+  float y = smoothstep(-.03, .03, sin(theta) * sin(phi));
+  float z = smoothstep(-.03, .03, cos(theta));
+  const float scale = 0.35;
   vec3 newPosition = position + vec3(x, y, z) * scale;
   vec4 worldPosition = modelViewMatrix * vec4(newPosition, 1.);
   gl_PointSize = (75. * (1. / -worldPosition.z) * noise) + 3.;
