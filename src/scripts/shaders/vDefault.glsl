fc1bb31e35087cae2f3ffc9233641604770fbf90
nmd <afarago.nmd@gmail.com>
2022-03-28 17:56:56 +0200

    MOD fancy anim.

--- src/scripts/shaders/vDefault.glsl
+++ src/scripts/shaders/vDefault.glsl
@@ -10,7 +10,7 @@ void main() {
   vUv = uv;
   vNoise = noise;
 
-  float alpha = (time / 2.) * (1. / noise);
+  float alpha = 10. * smoothstep(-5., 5., (time / 2. * (1. / noise)));
   vec3 displaceZ = vec3(.0, .0, smoothstep(-.03, .03, sin(alpha)) / 4.);
   vec4 worldPosition = modelViewMatrix * vec4(displaceZ + position, 1.);
   gl_PointSize = (75. * (1. / -worldPosition.z) * noise) + 3.;
